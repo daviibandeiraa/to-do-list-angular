9ec9c900fb97f5241dbc3a7e656ad3c70b510432
Davi Barbosa Bandeira <daviibandeiraa@gmail.com>
2024-04-09 18:39:08 -0300

    falta apenas aplicar o css ( funcionalidade - tudo ok )

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { Todo } from 'src/models/todos.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public todos: Todo[] = [];
+  public title: string = 'Minhas Tarefas';
+  public form: FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    });
+
+    this.load();
+  }
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save()
+    this.clear()
+  }
+
+  clear() {
+    this.form.reset()
+  }
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1)
+      this.todos.splice(index, 1)
+    this.save();
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+    this.save();
+  }
+  markAsUndone(todo: Todo) {
+    todo.done = false
+    this.save();
+  }
+  
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  }
+
+}
